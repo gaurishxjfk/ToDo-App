5b4ccfd09a7adc4db79b5c426885e95d6ccccc0a
Gaurish Naik <nerdgaurish@gmail.com>
2019-07-25 00:05:10 +0530

    Added validation and enabled striking tasks as completed

--- Todo.js
+++ Todo.js
@@ -7,10 +7,13 @@ class Todo extends Component {
         super(props)
             this.state ={
                 input : "",
-                tasks : []
+                tasks : [],
+                completed: this.completed
             } 
-    }     
+    }  
+
     
+
 //----------------------------------------------updatetext field----------------------------------------------
     update = (event) => {
         this.setState ({
@@ -35,24 +38,32 @@ class Todo extends Component {
             };
         });
     }
+//----------------------------------------------toggele checkbox----------------------------------------------
+    toggleCompleted = () => {
+        this.setState({
+            completed : !this.state.completed
+        })
+    }     
 
-    
 
-    render() {                
+    render() {   
+        var isDisabled = !this.state.input ? '!isDisabled':'';
+        var isComplete = this.state.completed ? 'complete' : '';           
+        var isEmpty = this.state.tasks.length === 0 ? 'No Tasks':' Currently there are ' +this.state.tasks.length+ ' tasks';  
         return (                
             <div>
                     <div>
                         <h1>ToDo App</h1>
-                        <p>Currently there are {this.state.tasks.length} tasks</p>
+                        <p>{isEmpty}</p>
                     </div>
                     <div>
                         <input type="text" placeholder="add task" value={this.state.input} onChange={this.update}></input>
-                        <button onClick={this.add}> Task #{this.state.tasks.length +1}</button>
+                        <button onClick={this.add} disabled={isDisabled}> Task #{this.state.tasks.length +1}</button>
                     </div>
                     <div>
                         <ul type="none">
                             {this.state.tasks.map((task, i) =>
-                                <li key={i} ><input type="checkbox"  />{task} <button data-index={i} onClick={this.dlit}>x</button></li>
+                                <li key={i} className={isComplete}><input type="checkbox" value={this.state.completed} onChange={() => this.toggleCompleted()} />{task} <button data-index={i} onClick={this.dlit}>x</button></li>
                             )}                           
                         </ul>
                         
@@ -62,4 +73,7 @@ class Todo extends Component {
         )
     }
 }
+
+
+
 export default Todo
